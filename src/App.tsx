d43d11ff0ec7db49cb611534f3b6a5b309ab265e
Serhan Coşkun <grafikri@gmail.com>
2019-08-10 18:17:56 +0300

    LoginForm test için ana component'e eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
+import RLogin from "./components/organisms/RLoginForm"
+
 const App: React.FC = () => {
-  return <div className="App">App</div>
+  return (
+    <div className="App">
+      <RLogin
+        formDisabled={false}
+        handleSubmit={(userName, password) => {
+          console.log("data: ", userName, password)
+        }}
+      />
+    </div>
+  )
 }
 
 export default App
