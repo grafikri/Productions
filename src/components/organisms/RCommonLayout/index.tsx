051429d6011a3d1d4b62157cc75c7a10fe132eb2
Serhan Coşkun <grafikri@gmail.com>
2019-08-10 20:25:30 +0300

    Header ve footer gibi alanların giriş yapıldıktan sonra sabit kalması için layout yapıldı

---
+++ src/components/organisms/RCommonLayout/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import RHeader from "../../organisms/RHeader"
+
+export default class RCommonLayout extends React.Component {
+  render() {
+    return (
+      <div className="o-r-common-layout">
+        <RHeader />
+        {this.props.children}
+      </div>
+    )
+  }
+}
