8c083878a7d9667146bcdce035f8465a0c6b6c2f
Serhan Coşkun <grafikri@gmail.com>
2019-08-10 19:52:35 +0300

    iç sayfalar için kullanılacak header tamamlandı.

--- src/components/organisms/RHeader/index.tsx
+++ src/components/organisms/RHeader/index.tsx
@@ -1,7 +1,20 @@
+import "./index.css"
 import React from "react"
+import { AppBar, Toolbar, Button } from "@material-ui/core"
 
 export default class RHeader extends React.Component {
   render() {
-    return <div>Header</div>
+    return (
+      <div className="o-r-header">
+        <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit">Ürünler</Button>
+            <Button color="inherit">Kategoriler</Button>
+            <div className="space" />
+            <Button color="inherit">Çıkış</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
   }
 }
