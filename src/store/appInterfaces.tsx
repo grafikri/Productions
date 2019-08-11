18caff97e950258989e9bc07b6baee9c3a5f25f4
Serhan Coşkun <grafikri@gmail.com>
2019-08-11 09:41:23 +0300

    Uygulama içindeki tüm data'ların şemalarını tutması için bir interface oluşturuldu

---
+++ src/store/appInterfaces.tsx
@@ -0,0 +1,13 @@
+/**
+ * Burada uygulama içinde kullanılan data'ların interface'leri bulunmaktadır
+ */
+
+/**
+ * Bir ürünün detayı
+ */
+export interface Product {
+  id: string
+  name: string
+  code: string
+  price: string
+}
