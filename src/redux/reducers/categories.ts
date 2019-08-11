6cddb74094890228c7e4fe4f193295528d5097a0
Serhan Coşkun <grafikri@gmail.com>
2019-08-11 18:47:15 +0300

    redux kullanarak eklenen yeni kategorinin state'e yazılması sağlandı

--- src/redux/reducers/categories.ts
+++ src/redux/reducers/categories.ts
@@ -1,14 +1,28 @@
-import { Reducer } from "redux"
+import { Reducer, Action } from "redux"
 import { Category } from "../../store/appInterfaces"
+import { types } from "../actionsTypes"
+import { randomRangeNumber, randomNumber } from "../../helpers"
 
-const initialState = [
-  { id: "0001", name: "Mazda", code: "M1" },
-  { id: "0002", name: "Opel", code: "O1" }
-]
-
-const categories: Reducer<Category[]> = (
-  state: Category[] = initialState,
-  action
-) => state
+const categories: Reducer<Category[]> = (state: Category[] = [], action) => {
+  switch (action.type) {
+    case types.ADD_NEW_CATEGORY:
+      const upperName = action.name as string
+      const id = randomRangeNumber(1000, 9999).toString()
+      const code =
+        (action.name[0] as string).toUpperCase() +
+        "_" +
+        randomRangeNumber(1000, 9999)
+      return (state = [
+        {
+          id: id,
+          name: upperName,
+          code: code
+        },
+        ...state
+      ])
+    default:
+      return state
+  }
+}
 
 export default categories
