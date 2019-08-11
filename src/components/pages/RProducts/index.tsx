e2bb050aebdec8b336582b502082ebf9be9f6e0f
Serhan Coşkun <grafikri@gmail.com>
2019-08-11 09:27:26 +0300

    Ürünler template ürünler sayfasının içine eklendi

---
+++ src/components/pages/RProducts/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import { default as RProductsTemplate } from "../../templates/RProducts"
+
+export default class RProducts extends React.Component {
+  render() {
+    return (
+      <div>
+        <RProductsTemplate
+          list={[{ name: "BMW1", code: "B_1", id: "1234" }]}
+          handleSubmit={data => {
+            console.log("products: ", data)
+          }}
+        />
+      </div>
+    )
+  }
+}
