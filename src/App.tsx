4f94a472c25c2bda85a5900ebbd200c4ccada3da
Serhan Coşkun <grafikri@gmail.com>
2019-08-11 22:45:15 +0300

    Temel router'lar eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
-//import RLogin from "./components/pages/RLogin"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+import RLogin from "./components/pages/RLogin"
 import RCategories from "./components/pages/RCategories"
+import RCategoryDetails from "./components/pages/RCategoryDetails"
+import RProducts from "./components/pages/RProducts"
+import RProductDetails from "./components/pages/RProductDetails"
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <RCategories />
+      <Router>
+        <Route path="/login" component={RLogin} />
+        <Route path="/categories" component={RCategories} />
+        <Route path="/category-details" component={RCategoryDetails} />
+        <Route path="/products" component={RProducts} />
+        <Route path="/product-details" component={RProductDetails} />
+      </Router>
     </div>
   )
 }
