b225228e9a29bdbcf0b76fe1f8b584d200b7e020
Serhan Coşkun <grafikri@gmail.com>
2019-08-11 23:19:44 +0300

    kategoriler ve ürünler linkleri aktifleştirildi

--- src/components/organisms/RHeader/index.tsx
+++ src/components/organisms/RHeader/index.tsx
@@ -1,15 +1,26 @@
 import "./index.css"
 import React from "react"
 import { AppBar, Toolbar, Button } from "@material-ui/core"
+import { withRouter } from "react-router-dom"
 
-export default class RHeader extends React.Component {
+class RHeader extends React.Component<any, any> {
   render() {
     return (
       <div className="o-r-header">
         <AppBar position="fixed">
           <Toolbar>
-            <Button color="inherit">Ürünler</Button>
-            <Button color="inherit">Kategoriler</Button>
+            <Button
+              color="inherit"
+              onClick={() => this.props.history.push("/products")}
+            >
+              Ürünler
+            </Button>
+            <Button
+              color="inherit"
+              onClick={() => this.props.history.push("/categories")}
+            >
+              Kategoriler
+            </Button>
             <div className="space" />
             <Button color="inherit">Çıkış</Button>
           </Toolbar>
@@ -18,3 +29,5 @@ export default class RHeader extends React.Component {
     )
   }
 }
+
+export default withRouter(RHeader)
