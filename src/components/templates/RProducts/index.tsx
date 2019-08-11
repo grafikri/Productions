a2ecf259c22abc30b83e65eb460126a4a2c9a6e2
Serhan Coşkun <grafikri@gmail.com>
2019-08-11 08:46:53 +0300

    Ürünlerin listeleneceği template sayfası tasarlanıyor.

---
+++ src/components/templates/RProducts/index.tsx
@@ -0,0 +1,70 @@
+import "./index.css"
+import React from "react"
+import CommonLayout from "../../organisms/RCommonLayout"
+import RAddSingleItem from "../../molecules/RAddSingleItem"
+import {
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody
+} from "@material-ui/core"
+
+/**
+ * Her bir ürünün içeriği
+ */
+export interface ProductInfo {
+  id: string
+  name: string
+  code: string
+}
+
+/**
+ * Props değerleri
+ */
+interface ProductsProps {
+  list: ProductInfo[]
+  handleSubmit(name: string): void
+}
+
+export default class RProducts extends React.Component<ProductsProps, any> {
+  render() {
+    return (
+      <div className="t-r-products">
+        <CommonLayout>
+          <div className="container">
+            <div className="formInput">
+              <RAddSingleItem
+                handleSubmit={this.props.handleSubmit}
+                placeHolder="Bir ürün adı girin"
+              />
+            </div>
+            <div className="products">
+              <Paper elevation={1}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Ad</TableCell>
+                      <TableCell align="right">Kod</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {this.props.list.map(item => (
+                      <TableRow key={item.id}>
+                        <TableCell component="th" scope="row">
+                          {item.name}
+                        </TableCell>
+                        <TableCell align="right">{item.code}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+            </div>
+          </div>
+        </CommonLayout>
+      </div>
+    )
+  }
+}
