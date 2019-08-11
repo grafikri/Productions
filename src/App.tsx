5fdcbfa86bbb8865c2fb90e2b8f7fc52e6789a45
Serhan Coşkun <grafikri@gmail.com>
2019-08-11 16:48:02 +0300

    Kategoriler sayfası denemek için app sayfasına eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
-import RLogin from "./components/pages/RLogin"
+//import RLogin from "./components/pages/RLogin"
+import RCategories from "./components/pages/RCategories"
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <RLogin />
+      <RCategories />
     </div>
   )
 }
