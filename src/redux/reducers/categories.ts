c319b518acb22b1925176bf329ee7998766ac4b4
Serhan Coşkun <grafikri@gmail.com>
2019-08-13 00:59:12 +0300

    Tüm kategorilerin silebilmek için reducer'a yeni bir case eklendi

--- src/redux/reducers/categories.ts
+++ src/redux/reducers/categories.ts
@@ -5,6 +5,9 @@ import { randomRangeNumber, randomNumber } from "../../helpers"
 
 const categories: Reducer<Category[]> = (state: Category[] = [], action) => {
   switch (action.type) {
+    case types.CLEAR_NEW_CATEGORY:
+      return []
+      break
     case types.ADD_NEW_CATEGORY:
       const upperName = action.name as string
       const id = randomRangeNumber(1000, 9999).toString()
