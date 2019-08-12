d032d6782c50df612e6a864be5105bf8719fee36
Serhan Coşkun <grafikri@gmail.com>
2019-08-12 14:40:11 +0300

    Kategori detaylarının gösterilmesi redux üzerinden sağlandı

--- src/components/pages/RCategoryDetails/index.tsx
+++ src/components/pages/RCategoryDetails/index.tsx
@@ -1,15 +1,20 @@
 import React from "react"
-
+import { connect } from "react-redux"
+import { ApplicationState } from "../../../store/appInterfaces"
 import { default as RCategoryDetailsTemplate } from "../../templates/RCategoryDetails"
 
-export default class RCategories extends React.Component {
+class RCategories extends React.Component<ReturnType<typeof mapStateToProps>> {
   render() {
     return (
       <div className="p-r-category-details">
-        <RCategoryDetailsTemplate
-          category={{ name: "Mazda", code: "O_1", id: "123" }}
-        />
+        <RCategoryDetailsTemplate category={this.props.category} />
       </div>
     )
   }
 }
+
+const mapStateToProps = (state: ApplicationState) => ({
+  category: state.category
+})
+
+export default connect(mapStateToProps)(RCategories)
