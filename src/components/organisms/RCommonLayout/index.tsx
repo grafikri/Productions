5443263d0002a06a79f434b40dc9b2e69b758c0b
Serhan Coşkun <grafikri@gmail.com>
2019-08-12 23:53:15 +0300

    Component'e loading özelliği eklendi. Artık loading değişkeninin durumuna göre içerikler görüntülenecek yada loading circle çıkartılacak

--- src/components/organisms/RCommonLayout/index.tsx
+++ src/components/organisms/RCommonLayout/index.tsx
@@ -1,13 +1,27 @@
 import "./index.css"
 import React from "react"
 import RHeader from "../../organisms/RHeader"
+import { CircularProgress } from "@material-ui/core"
 
-export default class RCommonLayout extends React.Component {
+interface RCommonLayoutProps {
+  loading?: boolean
+  errorMessage?: string
+}
+
+export default class RCommonLayout extends React.Component<RCommonLayoutProps> {
   render() {
     return (
       <div className="o-r-common-layout">
         <RHeader />
-        <div className="container">{this.props.children}</div>
+        <div className="container">
+          {this.props.loading === undefined || this.props.loading === true ? (
+            <CircularProgress size={24} />
+          ) : this.props.errorMessage === undefined ? (
+            this.props.children
+          ) : (
+            this.props.errorMessage
+          )}
+        </div>
       </div>
     )
   }
