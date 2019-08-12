3c68c30800e47233c1a6cff4b785c994a993f2e4
Serhan Coşkun <grafikri@gmail.com>
2019-08-13 01:00:32 +0300

    bugfix, error mesajı ve loading durumuna göre layout'un view görünümü güncellendi.

--- src/components/organisms/RCommonLayout/index.tsx
+++ src/components/organisms/RCommonLayout/index.tsx
@@ -1,7 +1,7 @@
 import "./index.css"
 import React from "react"
 import RHeader from "../../organisms/RHeader"
-import { CircularProgress } from "@material-ui/core"
+import { CircularProgress, Typography } from "@material-ui/core"
 
 interface RCommonLayoutProps {
   loading?: boolean
@@ -16,10 +16,16 @@ export default class RCommonLayout extends React.Component<RCommonLayoutProps> {
         <div className="container">
           {this.props.loading === undefined || this.props.loading === true ? (
             <CircularProgress size={24} />
-          ) : this.props.errorMessage === undefined ? (
+          ) : this.props.errorMessage === undefined ||
+            this.props.errorMessage === "" ? (
             this.props.children
           ) : (
-            this.props.errorMessage
+            <div>
+              <Typography color="error">
+                {this.props.errorMessage}. Lütfen sayfayı yenileyerek tekrar
+                deneyin.
+              </Typography>
+            </div>
           )}
         </div>
       </div>
