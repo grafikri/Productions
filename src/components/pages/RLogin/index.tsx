7de31418bd61f4679b18dd4dc6de3c64c9e4b044
Serhan Coşkun <grafikri@gmail.com>
2019-08-12 11:41:12 +0300

    Oymak Api'ye component üzerinden bağlantı sağlanıyor

--- src/components/pages/RLogin/index.tsx
+++ src/components/pages/RLogin/index.tsx
@@ -1,7 +1,23 @@
 import React from "react"
+import { connect } from "react-redux"
+import { Dispatch } from "redux"
+import OymakApi from "../../../services/oymakApi"
+
 import { default as RLoginTemplate } from "../../templates/RLogin"
 
-export default class RLogin extends React.Component {
+class RLogin extends React.Component<
+  ReturnType<typeof mapDispatchToProps>,
+  any
+> {
+  componentDidMount() {
+    OymakApi.login("serhan.coskun", "Dev9156.")
+      .then(response => {
+        console.log("success: ", response)
+      })
+      .catch(function(error) {
+        console.log("error: ", error)
+      })
+  }
   render() {
     return (
       <div>
@@ -14,3 +30,13 @@ export default class RLogin extends React.Component {
     )
   }
 }
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getAllFiles: (): void => {
+    setTimeout(() => {
+      console.log("TimeOut complate")
+    }, 1000)
+  }
+})
+
+export default connect(mapDispatchToProps)(RLogin)
