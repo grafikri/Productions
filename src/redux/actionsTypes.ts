9b8b3c06a276c71a3c857f5601da392a2f116117
Serhan Coşkun <grafikri@gmail.com>
2019-08-13 00:53:50 +0300

    uygulama genelinde kullanılmak üzere type değerleri eklendi. Bu değerler actions ve reducer'lar tarafından  karşılanacaktır.

--- src/redux/actionsTypes.ts
+++ src/redux/actionsTypes.ts
@@ -6,4 +6,12 @@ export enum types {
   ADD_NEW_PRODUCT,
   SET_CATEGORY,
   SET_PRODUCT
+  /**
+   * Layout'un yüklenme durumunu güncellemek için kullanılır
+   */
+  UPDATE_LAYOUT_LOADING,
+  /**
+   * Layout yüklenirken bir hata meydana gelirse hata mesajını belirtmek için kullanılır
+   */
+  UPDATE_LAYOUT_ERROR_MESSAGE
 }
