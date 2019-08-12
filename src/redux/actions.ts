78d1ebe0d1881fa87f29c8f3a7ccafac72e463bd
Serhan Coşkun <grafikri@gmail.com>
2019-08-13 00:51:49 +0300

    componentlerin yüklenme durumlarını ve hata mesajlarını kontrol eden action'lar eklendi

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -9,3 +9,13 @@ export const addNewProduct = (name: string) => ({
   type: types.ADD_NEW_PRODUCT,
   name: name
 })
+
+export const updateLayoutLoading = (situation: boolean) => ({
+  type: types.UPDATE_LAYOUT_LOADING,
+  situation: situation
+})
+
+export const updateLayoutErrorMessage = (message: string) => ({
+  type: types.UPDATE_LAYOUT_ERROR_MESSAGE,
+  message: message
+})
