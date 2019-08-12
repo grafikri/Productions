22d8dc1e45177c3cab0b734db53e9eba06cfd800
Serhan Coşkun <grafikri@gmail.com>
2019-08-12 23:51:30 +0300

    - Api'den dönen yanıtlar için interface'ler eklendi - Kategori listesini çekerken şimdilik sunucu bağlantısı yerine bağlantı simule edildi.

--- src/services/oymakApi.tsx
+++ src/services/oymakApi.tsx
@@ -1,4 +1,3 @@
-import * as Api from "../store/oymakApiInterfaces"
 import axios from "axios"
 /**
  * Oymak Grup Api'si ile ilgili tüm bağlantılar buradan sağlanmaktadır
@@ -19,8 +18,8 @@ export default class OymakApi {
     }
   })
 
-  static login(userName: string, password: string): Promise<Api.Login> {
-    return new Promise<Api.Login>((resolve, reject) => {
+  static login(userName: string, password: string): Promise<Login> {
+    return new Promise((resolve, reject) => {
       return this.instance.post("login", {
         username: userName,
         password: password,
@@ -39,4 +38,79 @@ export default class OymakApi {
       // }, 1000)
     })
   }
+
+  static getCategoryList(): Promise<CategoryList> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          list: [
+            {
+              Id: "ee33aa62-8986-45b0-b46d-17c019c50e27",
+              Name: "Mazda"
+            },
+            {
+              Id: "ef587afd-f937-4b73-b5cd-88d4f913249c",
+              Name: "Opel"
+            },
+            {
+              Id: "c1dcb2f1-6870-4fb1-aacf-c90aa92fc136",
+              Name: "Opel"
+            }
+          ]
+        })
+      }, 2000)
+    })
+  }
+}
+
+/**
+ * Api'den alınan response'ları temsil eden interface'lerdir
+ */
+
+/**
+ * Login response
+ */
+interface Login {
+  access_token: string
+  token_type: string
+  expires_in: number
+  userName: string
+  ".issued": string
+  ".expires": string
+}
+
+/**
+ * Kategori bilgileri
+ */
+interface CategoryCard {
+  Id: string
+  Code: string
+  Name: string
+  Products: CategoryCardProduct[]
+}
+
+/**
+ * Kategori altındaki ürünler
+ */
+interface CategoryCardProduct {
+  Id: string
+  Code: string
+  Name: string
+  ExpiredDate: string
+  Price: number
+}
+
+/**
+ * Kategoriler
+ */
+interface CategoryList {
+  list: CategoryListItem[]
+}
+
+/**
+ * Kategoriler listesindeki her item değeri
+ */
+interface CategoryListItem {
+  Id: string
+  Name: string
 }
