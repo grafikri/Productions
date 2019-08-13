b4b4198cc121295f27b0d0a3df3519310ced2c99
Serhan Coşkun <grafikri@gmail.com>
2019-08-13 20:08:35 +0300

    yeni kategori eklemek için reducer tarafında düzenleme yapıldı

--- src/redux/reducers/categories.ts
+++ src/redux/reducers/categories.ts
@@ -1,7 +1,6 @@
 import { Reducer, Action } from "redux"
 import { Category } from "../../store/appInterfaces"
 import { types } from "../actionsTypes"
-import { randomRangeNumber, randomNumber } from "../../helpers"
 
 const categories: Reducer<Category[]> = (state: Category[] = [], action) => {
   switch (action.type) {
@@ -10,20 +9,7 @@ const categories: Reducer<Category[]> = (state: Category[] = [], action) => {
     case types.CLEAR_CATEGORIES:
       return []
     case types.ADD_NEW_CATEGORY:
-      const upperName = action.name as string
-      const code =
-        (action.name[0] as string).toUpperCase() +
-        "_" +
-        randomRangeNumber(1000, 9999)
-      return (state = [
-        {
-          id: action.id,
-          name: upperName,
-          code: code,
-          products: []
-        },
-        ...state
-      ])
+      return [action.category, ...state]
     default:
       return state
   }
