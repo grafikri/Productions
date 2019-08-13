e44f9233045dea06c802ec7e772dc3be0baabce2
Serhan Coşkun <grafikri@gmail.com>
2019-08-13 12:20:07 +0300

    CLEAR_NEW_CATEGORY -> CLEAR_CATEGORIES şeklinde değiştirildi. (Daha mantıklı)

--- src/redux/reducers/categories.ts
+++ src/redux/reducers/categories.ts
@@ -5,7 +5,7 @@ import { randomRangeNumber, randomNumber } from "../../helpers"
 
 const categories: Reducer<Category[]> = (state: Category[] = [], action) => {
   switch (action.type) {
-    case types.CLEAR_NEW_CATEGORY:
+    case types.CLEAR_CATEGORIES:
       return []
       break
     case types.ADD_NEW_CATEGORY:
