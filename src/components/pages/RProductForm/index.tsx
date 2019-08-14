4c28d3fbe1fa8e39f824d5d9c2ca2badf8d68bb8
Serhan Coşkun <grafikri@gmail.com>
2019-08-14 21:25:26 +0300

    ürün ekleme sayfası oluşturuldu

---
+++ src/components/pages/RProductForm/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { connect } from "react-redux"
+//import RProductForm from "./components/templates/RProductForm"
+class RProductForm extends React.Component {
+  render() {
+    return <div className="p-r-product-form">Form page</div>
+  }
+}
+
+export default RProductForm
