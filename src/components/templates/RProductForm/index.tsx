b18516a82ce7d03bb4c20138c88fa243cb982b8b
Serhan Coşkun <grafikri@gmail.com>
2019-08-14 21:22:29 +0300

    Ürün'nün sunucuya kaydından sonra hata yada olumlu sonuca göre ekrana mesaj çıkartmak için modal component'i eklendi

--- src/components/templates/RProductForm/index.tsx
+++ src/components/templates/RProductForm/index.tsx
@@ -3,12 +3,25 @@ import React from "react"
 import CommonLayout from "../../organisms/RCommonLayout"
 import { LayoutErrorProps } from "../../../store/appInterfaces"
 import RAddNewProductForm from "../../molecules/RAddNewProductForm"
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button
+} from "@material-ui/core"
 /**
  * Props değerleri
  */
-interface RProductFormProps extends LayoutErrorProps {}
+interface RProductFormProps extends LayoutErrorProps {
+  dialogTitle: string
+  dialogDesc: string
+  dialogOpen: boolean
+}
 
 export default class RProductForm extends React.Component<RProductFormProps> {
+  handleClose() {}
   render() {
     return (
       <div className="t-r-product-form">
@@ -23,6 +36,23 @@ export default class RProductForm extends React.Component<RProductFormProps> {
               console.log("form: ", form)
             }}
           />
+
+          <Dialog
+            open={this.props.dialogOpen}
+            onClose={this.handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle>{this.props.dialogTitle}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>{this.props.dialogDesc}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary" autoFocus>
+                Tamam
+              </Button>
+            </DialogActions>
+          </Dialog>
         </CommonLayout>
       </div>
     )
