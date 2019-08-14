b0970dc1345c338b1add0f78e4b295847ce9fc35
Serhan Coşkun <grafikri@gmail.com>
2019-08-14 19:07:36 +0300

    ürün ekleme formu için taslak çıkarıldı

--- src/components/molecules/RAddNewProductForm/index.tsx
+++ src/components/molecules/RAddNewProductForm/index.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import {
   TextField,
   FormControl,
-  InputBase,
+  MenuItem,
   Button,
   Paper
 } from "@material-ui/core"
@@ -13,6 +13,11 @@ import {
  */
 interface RAddSingleItemStates {
   name: string
+  date: string
+  time: string
+  price: string
+  caregoryCode: string
+  formDisabled: boolean
   buttonDisabled: boolean
 }
 
@@ -30,10 +35,16 @@ export default class RAddSingleItem extends React.Component<
 > {
   state = {
     name: "",
+    date: "",
+    time: "",
+    price: "",
+    caregoryCode: "",
+    formDisabled: false,
     buttonDisabled: true
   }
 
   render() {
+    const dense = {}
     return (
       <div className="m-r-add-new-product-form">
         <Paper>
@@ -47,17 +58,91 @@ export default class RAddSingleItem extends React.Component<
             <div className="container">
               <div className="left">
                 <FormControl fullWidth>
-                  <InputBase
-                    placeholder={this.props.placeHolder}
+                  <TextField
+                    variant="outlined"
+                    label="Ürün adı"
+                    placeholder="Ürün adı"
+                    margin="normal"
                     value={this.state.name}
-                    inputProps={{ "aria-label": "naked" }}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
                     onChange={event => {
                       this.setState({
-                        name: event.target.value,
-                        buttonDisabled: event.target.value.trim() === ""
+                        name: event.target.value
                       })
                     }}
                   />
+                  <TextField
+                    variant="outlined"
+                    label="Fiyat"
+                    placeholder="Fiyat"
+                    margin="normal"
+                    type="number"
+                    value={this.state.price}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    onChange={event => {
+                      this.setState({
+                        price: event.target.value
+                      })
+                    }}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Son geçerlilik tarihi"
+                    placeholder="Son geçerlilik tarihi"
+                    margin="normal"
+                    type="date"
+                    value={this.state.date}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    onChange={event => {
+                      this.setState({
+                        date: event.target.value
+                      })
+                    }}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Son geçerlilik saati"
+                    placeholder="Son geçerlilik saati"
+                    margin="normal"
+                    type="time"
+                    value={this.state.time}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    onChange={event => {
+                      this.setState({
+                        time: event.target.value
+                      })
+                    }}
+                  />
+                  <TextField
+                    select
+                    label="Ürün kategorisi seçin"
+                    placeholder="Ürün kategorisi seçin"
+                    value={this.state.caregoryCode}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    onChange={event => {
+                      this.setState({
+                        caregoryCode: event.target.value
+                      })
+                    }}
+                    margin="normal"
+                    variant="outlined"
+                  >
+                    {[{ value: "O_1234", label: "Opel" }].map(option => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
                 </FormControl>
               </div>
               <div className="right">
