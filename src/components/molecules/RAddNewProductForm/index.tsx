f00610b394c0e6aa5b8109d823154c0ea70ac6a1
Serhan Coşkun <grafikri@gmail.com>
2019-08-14 20:48:32 +0300

    submit edilecek form interface'i belirlendi

--- src/components/molecules/RAddNewProductForm/index.tsx
+++ src/components/molecules/RAddNewProductForm/index.tsx
@@ -50,120 +50,128 @@ export default class RAddSingleItem extends React.Component<
   }
 
   render() {
+    const form: Form = { ...this.state }
+
     return (
       <div className="m-r-add-new-product-form">
-        <Paper>
-          <form
-            onSubmit={e => {
-              e.preventDefault()
-              this.props.handleSubmit(this.state.name)
-              this.setState({ name: "", buttonDisabled: true })
-            }}
-          >
-            <div className="container">
-              <div className="left">
-                <FormControl fullWidth>
-                  <TextField
-                    variant="outlined"
-                    label="Ürün adı"
-                    placeholder="Ürün adı"
-                    margin="normal"
-                    value={this.state.name}
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    onChange={event => {
-                      this.setState({
-                        name: event.target.value
-                      })
-                    }}
-                  />
-                  <TextField
-                    variant="outlined"
-                    label="Fiyat"
-                    placeholder="Fiyat"
-                    margin="normal"
-                    type="number"
-                    value={this.state.price}
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    onChange={event => {
-                      this.setState({
-                        price: event.target.value
-                      })
-                    }}
-                  />
-                  <TextField
-                    variant="outlined"
-                    label="Son geçerlilik tarihi"
-                    placeholder="Son geçerlilik tarihi"
-                    margin="normal"
-                    type="date"
-                    value={this.state.date}
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    onChange={event => {
-                      this.setState({
-                        date: event.target.value
-                      })
-                    }}
-                  />
-                  <TextField
-                    variant="outlined"
-                    label="Son geçerlilik saati"
-                    placeholder="Son geçerlilik saati"
-                    margin="normal"
-                    type="time"
-                    value={this.state.time}
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    onChange={event => {
-                      this.setState({
-                        time: event.target.value
-                      })
-                    }}
-                  />
-                  <TextField
-                    select
-                    label="Ürün kategorisi seçin"
-                    placeholder="Ürün kategorisi seçin"
-                    value={this.state.caregoryCode}
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    onChange={event => {
-                      this.setState({
-                        caregoryCode: event.target.value
-                      })
-                    }}
-                    margin="normal"
-                    variant="outlined"
-                  >
-                    {this.props.categories.map(option => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                </FormControl>
-              </div>
-              <div className="right">
-                <Button
-                  disabled={this.state.buttonDisabled}
-                  variant="contained"
-                  color="primary"
-                  size="large"
-                  type="submit"
+        <form
+          onSubmit={e => {
+            e.preventDefault()
+            this.props.handleSubmit(form)
+            //this.setState({ name: "", buttonDisabled: true })
+          }}
+        >
+          <div className="container">
+            <div className="left">
+              <FormControl fullWidth>
+                <TextField
+                  variant="outlined"
+                  label="Ürün adı"
+                  placeholder="Ürün adı"
+                  margin="normal"
+                  value={this.state.name}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={event => {
+                    this.setState({
+                      name: event.target.value
+                    })
+                  }}
+                />
+                <TextField
+                  variant="outlined"
+                  label="Fiyat"
+                  placeholder="Fiyat"
+                  margin="normal"
+                  type="number"
+                  value={this.state.price}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={event => {
+                    this.setState({
+                      price: event.target.value
+                    })
+                  }}
+                />
+                <TextField
+                  variant="outlined"
+                  label="Son geçerlilik tarihi"
+                  placeholder="Son geçerlilik tarihi"
+                  margin="normal"
+                  type="date"
+                  value={this.state.date}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={event => {
+                    this.setState({
+                      date: event.target.value
+                    })
+                  }}
+                />
+                <TextField
+                  variant="outlined"
+                  label="Son geçerlilik saati"
+                  placeholder="Son geçerlilik saati"
+                  margin="normal"
+                  type="time"
+                  value={this.state.time}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={event => {
+                    this.setState({
+                      time: event.target.value
+                    })
+                  }}
+                />
+                <TextField
+                  select
+                  label="Ürün kategorisi seçin"
+                  placeholder="Ürün kategorisi seçin"
+                  value={this.state.caregoryCode}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={event => {
+                    this.setState({
+                      caregoryCode: event.target.value
+                    })
+                  }}
+                  margin="normal"
+                  variant="outlined"
                 >
-                  Ekle
-                </Button>
-              </div>
+                  {this.props.categories.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </FormControl>
             </div>
-          </form>
-        </Paper>
+            <div className="right">
+              <Button
+                disabled={
+                  this.state.name == "" ||
+                  this.state.date == "" ||
+                  this.state.time == "" ||
+                  this.state.price == "" ||
+                  this.state.caregoryCode == ""
+                    ? true
+                    : false
+                }
+                variant="contained"
+                color="primary"
+                size="large"
+                type="submit"
+              >
+                Ekle
+              </Button>
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
