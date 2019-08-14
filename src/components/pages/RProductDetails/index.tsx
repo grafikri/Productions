05a9486a5c484d5b340ab04106342849c87298bf
Serhan Coşkun <grafikri@gmail.com>
2019-08-14 16:31:04 +0300

    thunk üzerindeki kategori detaylarını getiren method component'e dahil edildi

--- src/components/pages/RProductDetails/index.tsx
+++ src/components/pages/RProductDetails/index.tsx
@@ -3,9 +3,10 @@ import React from "react"
 import { default as RProductDetailsTemplate } from "../../templates/RProductDetails"
 import { ApplicationState } from "../../../store/appInterfaces"
 import { connect } from "react-redux"
+import { fetchProductCard } from "../../../thunk"
 
 class RProductDetails extends React.Component<
-  ReturnType<typeof mapStateToProps>
+  ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>
 > {
   render() {
     return (
@@ -20,4 +21,13 @@ const mapStateToProps = (state: ApplicationState) => ({
   product: state.product
 })
 
-export default connect(mapStateToProps)(RProductDetails)
+const mapDispatchToProps = (dispatch: any) => ({
+  getProductDetails: (id: string) => {
+    dispatch(fetchProductCard(id))
+  }
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RProductDetails)
