cdbe1de692d22da665ed17c596daa9f5e630ba38
Serhan Coşkun <grafikri@gmail.com>
2019-08-14 22:34:28 +0300

    api'den dönen sonuçlara göre ürün ekleme sayfası yönetildi

--- src/components/pages/RProductForm/index.tsx
+++ src/components/pages/RProductForm/index.tsx
@@ -1,16 +1,20 @@
 import React from "react"
 import { connect } from "react-redux"
 import { default as RProductFormTemplate } from "../../templates/RProductForm"
-class RProductForm extends React.Component {
+import {
+  ApplicationState,
+  ConnectionSituations
+} from "../../../store/appInterfaces"
+class RProductForm extends React.Component<ReturnType<typeof mapStateToProps>> {
   render() {
     return (
       <div className="p-r-product-form">
         <RProductFormTemplate
-          formSaving={false}
-          formSaveResult={true}
-          dialogTitle="Kayıt başarılı"
-          dialogDesc="Ürün sunucuya kayıt edildi. Aşağıdaki butona dokunduğunuzda ürün sayfasına yönlendirileceksiniz."
-          dialogOpen={false}
+          formSaving={this.props.formSaving}
+          formSaveResult={this.props.formSaveResult}
+          dialogTitle={this.props.dialogTitle()}
+          dialogDesc={this.props.dialogDesc()}
+          dialogOpen={this.props.dialogOpen}
           handleSubmit={form => {
             console.log("form: ", form)
           }}
@@ -20,4 +24,38 @@ class RProductForm extends React.Component {
   }
 }
 
-export default RProductForm
+const mapStateToProps = (state: ApplicationState) => ({
+  formSaveResult:
+    state.application.connectionStatus == ConnectionSituations.DONE,
+  formSaving:
+    state.application.connectionStatus == ConnectionSituations.RUNNING
+      ? true
+      : false,
+  dialogTitle: (): string => {
+    switch (state.application.connectionStatus) {
+      case ConnectionSituations.DONE:
+        return "Kayıt başarılı"
+      case ConnectionSituations.FAIL:
+        return "Başarısız"
+      case ConnectionSituations.RUNNING:
+        return ""
+      default:
+        return "-"
+    }
+  },
+  dialogDesc: (): string => {
+    switch (state.application.connectionStatus) {
+      case ConnectionSituations.DONE:
+        return "Ürün sunucuya kayıt edildi. Aşağıdaki butona dokunduğunuzda ürün sayfasına yönlendirileceksiniz."
+      case ConnectionSituations.FAIL:
+        return state.application.connectionErrorMessage
+      case ConnectionSituations.RUNNING:
+        return ""
+      default:
+        return "-"
+    }
+  },
+  dialogOpen: state.application.connectionStatus != ConnectionSituations.RUNNING
+})
+
+export default connect(mapStateToProps)(RProductForm)
