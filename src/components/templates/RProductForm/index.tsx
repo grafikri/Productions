57908e758f6d654a937f33a023260dae897be5df
Serhan Coşkun <grafikri@gmail.com>
2019-08-14 20:51:50 +0300

    yeni ürün eklemek için template oluşturuldu. ürün ekleme formu component'e dahil edildi

---
+++ src/components/templates/RProductForm/index.tsx
@@ -0,0 +1,32 @@
+//import "./index.css"
+import React from "react"
+import CommonLayout from "../../organisms/RCommonLayout"
+import { LayoutErrorProps } from "../../../store/appInterfaces"
+import RAddNewProductForm from "../../molecules/RAddNewProductForm"
+/**
+ * Props değerleri
+ */
+interface RProductFormProps extends LayoutErrorProps {}
+
+export default class RProductForm extends React.Component<
+  RProductFormProps,
+  any
+> {
+  render() {
+    return (
+      <div className="t-r-product-form">
+        <CommonLayout loading={false} errorMessage={""}>
+          <RAddNewProductForm
+            categories={[
+              { value: "O_1234", label: "Opel" },
+              { value: "M_1234", label: "Mazda" }
+            ]}
+            handleSubmit={form => {
+              console.log("form: ", form)
+            }}
+          />
+        </CommonLayout>
+      </div>
+    )
+  }
+}
