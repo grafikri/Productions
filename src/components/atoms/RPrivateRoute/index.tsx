e36f530342d25e6f2d0e9dcaebbcb19b9fad570d
Serhan Coşkun <grafikri@gmail.com>
2019-08-15 20:44:11 +0300

    auth user'ın login olup olmadığını state üzerinden component'e aktarıp gerekli olduğu durumda redirect yönlendirmesi yapıldı

--- src/components/atoms/RPrivateRoute/index.tsx
+++ src/components/atoms/RPrivateRoute/index.tsx
@@ -1,18 +1,22 @@
 import React, { Component } from "react"
+import { connect } from "react-redux"
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
+import { ApplicationState } from "../../../store/appInterfaces"
 
-class PrivateRoute extends React.Component<{
-  path: string
-}> {
+class PrivateRoute extends React.Component<
+  {
+    path: string
+  } & ReturnType<typeof mapStateToProps>
+> {
   render() {
     return (
       <Route
         path={this.props.path}
         render={props => {
-          return 100 == 100 ? (
-            <Redirect to={{ pathname: "/login" }} />
-          ) : (
+          return this.props.isLogin ? (
             this.props.children
+          ) : (
+            <Redirect to={{ pathname: "/login" }} />
           )
         }}
       />
@@ -20,4 +24,8 @@ class PrivateRoute extends React.Component<{
   }
 }
 
-export default PrivateRoute
+const mapStateToProps = (state: ApplicationState) => ({
+  isLogin: state.auth.token == "" ? false : true
+})
+
+export default connect(mapStateToProps)(PrivateRoute)
