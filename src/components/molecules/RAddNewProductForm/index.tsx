eeec94bef5d334ee1e08d8b9177514d275c9e97a
Serhan Coşkun <grafikri@gmail.com>
2019-08-15 10:45:39 +0300

    sınıfın adı güncellendi

--- src/components/molecules/RAddNewProductForm/index.tsx
+++ src/components/molecules/RAddNewProductForm/index.tsx
@@ -46,7 +46,7 @@ export interface Form {
   caregoryCode: string
 }
 
-export default class RAddSingleItem extends React.Component<
+export default class RAddNewProductForm extends React.Component<
   RAddNewProductFormProps,
   RAddNewProductFormStates
 > {
