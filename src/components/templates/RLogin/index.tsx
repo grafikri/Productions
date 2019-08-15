9ab28ec67b2b626805d6991866aed88e44fc2d3c
Serhan Coşkun <grafikri@gmail.com>
2019-08-15 13:42:44 +0300

    Login işleminin sonucunu görüntülemek için dialog eklendi

--- src/components/templates/RLogin/index.tsx
+++ src/components/templates/RLogin/index.tsx
@@ -1,10 +1,22 @@
 import "./index.css"
 import React from "react"
 import RLoginForm from "../../organisms/RLoginForm"
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button
+} from "@material-ui/core"
 
 interface RLoginProps {
   handleSubmit(userName: string, password: string): void
   formDisabled: boolean
+  dialogTitle: string
+  dialogDesc: string
+  dialogOpen: boolean
+  handleClose(): void
 }
 
 export default class RLogin extends React.Component<RLoginProps, {}> {
@@ -16,6 +28,30 @@ export default class RLogin extends React.Component<RLoginProps, {}> {
             formDisabled={this.props.formDisabled}
             handleSubmit={this.props.handleSubmit}
           />
+          <Dialog
+            open={this.props.dialogOpen}
+            onClose={() => {
+              this.props.handleClose()
+            }}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle>{this.props.dialogTitle}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>{this.props.dialogDesc}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => {
+                  this.props.handleClose()
+                }}
+                color="primary"
+                autoFocus
+              >
+                Tamam
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
     )
