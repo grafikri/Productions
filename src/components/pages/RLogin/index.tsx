708eae09a5e3a159324dfa155391dd0f0e55d859
Serhan Coşkun <grafikri@gmail.com>
2019-08-15 13:35:07 +0300

    oymak api burada kullanılmayacağı için kaldırıldı

--- src/components/pages/RLogin/index.tsx
+++ src/components/pages/RLogin/index.tsx
@@ -1,42 +1,33 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Dispatch } from "redux";
-import OymakApi from "../../../services/oymakApi";
+import React from "react"
+import { connect } from "react-redux"
+import { Dispatch } from "redux"
 
-import { default as RLoginTemplate } from "../../templates/RLogin";
+import { default as RLoginTemplate } from "../../templates/RLogin"
 
 class RLogin extends React.Component<
   ReturnType<typeof mapDispatchToProps>,
   any
 > {
-  componentDidMount() {
-    // OymakApi.login("serhan.coskun", "Dev9156.")
-    //   .then(response => {
-    //     console.log("success: ", response)
-    //   })
-    //   .catch(function(error) {
-    //     console.log("error: ", error)
-    //   })
-  }
+  componentDidMount() {}
   render() {
     return (
       <div>
         <RLoginTemplate
           handleSubmit={(userName, password) => {
-            console.log("data: ", userName, password);
+            console.log("data: ", userName, password)
           }}
         />
       </div>
-    );
+    )
   }
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   getAllFiles: (): void => {
     setTimeout(() => {
-      console.log("TimeOut complate");
-    }, 1000);
+      console.log("TimeOut complate")
+    }, 1000)
   }
-});
+})
 
-export default connect(mapDispatchToProps)(RLogin);
+export default connect(mapDispatchToProps)(RLogin)
