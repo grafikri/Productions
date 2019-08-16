aca9f5d90d99b1dc9a6676833f5c1e90c496863f
Serhan Coşkun <grafikri@gmail.com>
2019-08-16 10:48:28 +0300

    filtre elementlerinin hizalanması sağlandı

--- src/components/molecules/RCategoryFilter/index.tsx
+++ src/components/molecules/RCategoryFilter/index.tsx
@@ -1,7 +1,14 @@
 import "./index.css"
 import React from "react"
 import { Category } from "../../../store/appInterfaces"
-import { TextField, MenuItem } from "@material-ui/core"
+import {
+  TextField,
+  MenuItem,
+  Button,
+  FormGroup,
+  FormControl,
+  Grid
+} from "@material-ui/core"
 
 export default class RCatergoryFilter extends React.Component<{
   list: Category[]
@@ -20,50 +27,83 @@ export default class RCatergoryFilter extends React.Component<{
           }}
         >
           <div className="form-container">
-            <TextField
-              select
-              label="Kategori"
-              placeholder="Kategori"
-              value={this.state.name}
-              InputLabelProps={{
-                shrink: true
-              }}
-              onChange={event => {
-                this.setState({
-                  name: event.target.value
-                })
-              }}
-              margin="normal"
-              variant="outlined"
-            >
-              {this.props.list.map(option => (
-                <MenuItem key={option.id} value={option.name}>
-                  {option.name}
-                </MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              select
-              label="Kod"
-              placeholder="Kod"
-              value={this.state.code}
-              InputLabelProps={{
-                shrink: true
-              }}
-              onChange={event => {
-                this.setState({
-                  code: event.target.value
-                })
-              }}
-              margin="normal"
-              variant="outlined"
-            >
-              {this.props.list.map(option => (
-                <MenuItem key={option.id} value={option.code}>
-                  {option.code}
-                </MenuItem>
-              ))}
-            </TextField>
+            <Grid container spacing={3}>
+              <Grid item xs={5}>
+                <TextField
+                  fullWidth
+                  select
+                  label="Kategori"
+                  placeholder="Kategori"
+                  value={this.state.name}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={event => {
+                    this.setState({
+                      name: event.target.value
+                    })
+                  }}
+                  margin="normal"
+                  variant="outlined"
+                >
+                  {this.props.list.map(option => (
+                    <MenuItem key={option.id} value={option.name}>
+                      {option.name}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={5}>
+                <TextField
+                  fullWidth
+                  select
+                  label="Kod"
+                  placeholder="Kod"
+                  value={this.state.code}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={event => {
+                    this.setState({
+                      code: event.target.value
+                    })
+                  }}
+                  margin="normal"
+                  variant="outlined"
+                >
+                  {this.props.list.map(option => (
+                    <MenuItem key={option.id} value={option.code}>
+                      {option.code}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={2}>
+                <Grid
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                  style={{ height: "100%" }}
+                >
+                  <Grid item xs={12}>
+                    <Button
+                      fullWidth
+                      disabled={
+                        this.state.name == "" || this.state.code == ""
+                          ? true
+                          : false
+                      }
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                    >
+                      Filtrele
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
           </div>
         </form>
       </div>
