1ee8653eb34fcc0623a259b3813c8f88722f09c6
Serhan Coşkun <grafikri@gmail.com>
2019-08-16 10:21:15 +0300

    Kategori ve code selectbox'ları eklendş

--- src/components/molecules/RCategoryFilter/index.tsx
+++ src/components/molecules/RCategoryFilter/index.tsx
@@ -1,9 +1,72 @@
+import "./index.css"
 import React from "react"
+import { Category } from "../../../store/appInterfaces"
+import { TextField, MenuItem, FormControl } from "@material-ui/core"
 
-import {} from "@material-ui/core"
+export default class RCatergoryFilter extends React.Component<{
+  list: Category[]
+}> {
+  state = {
+    name: "",
+    code: ""
+  }
 
-export default class RCatergoryFilter extends React.Component {
   render() {
-    return <div className="t-r-category-filter">filter</div>
+    return (
+      <div className="t-r-category-filter">
+        <form
+          onSubmit={e => {
+            e.preventDefault()
+          }}
+        >
+          <div className="form-container">
+            <TextField
+              select
+              label="Kategori"
+              placeholder="Kategori"
+              value={this.state.name}
+              InputLabelProps={{
+                shrink: true
+              }}
+              onChange={event => {
+                this.setState({
+                  name: event.target.value
+                })
+              }}
+              margin="normal"
+              variant="outlined"
+            >
+              {this.props.list.map(option => (
+                <MenuItem key={option.id} value={option.name}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              select
+              label="Kod"
+              placeholder="Kod"
+              value={this.state.code}
+              InputLabelProps={{
+                shrink: true
+              }}
+              onChange={event => {
+                this.setState({
+                  code: event.target.value
+                })
+              }}
+              margin="normal"
+              variant="outlined"
+            >
+              {this.props.list.map(option => (
+                <MenuItem key={option.id} value={option.code}>
+                  {option.code}
+                </MenuItem>
+              ))}
+            </TextField>
+          </div>
+        </form>
+      </div>
+    )
   }
 }
