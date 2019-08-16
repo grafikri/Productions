3e2f2acd3d67d2ecc512a76667e261ab3b6ef7a3
Serhan Coşkun <grafikri@gmail.com>
2019-08-16 09:59:55 +0300

    Kategorileri filtreleme için filter component'i oluşturuldu

---
+++ src/components/molecules/RCategoryFilter/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+import {} from "@material-ui/core"
+
+export default class RCatergoryFilter extends React.Component {
+  render() {
+    return <div className="t-r-category-filter">filter</div>
+  }
+}
